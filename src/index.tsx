00414c2b32e8f517211e1be116307354fbdc8487
Roman Kornev <kornev.roma62@gmail.com>
2023-04-18 13:52:58 +0300

    fix: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "@styles/index.scss";
-import { AppRoutes } from "src/routes/AppRoutes";
+import { AppRoutes } from "@routes/AppRoutes";
 import { store } from "@redux/store";
 import { Provider } from "react-redux";
 
